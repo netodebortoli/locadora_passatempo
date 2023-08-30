dd89b1e035e768dfc8fc92df297376fa631dc82d
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-08-30 15:34:11 -0300

    Iniciado backend do projeto locadora

---
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/BackendLocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.locadora.backendlocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLocadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLocadoraApplication.class, args);
+    }
+
+}
---
+++ backend-locadora/src/test/java/com/locadora/backendlocadora/BackendLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadora.backendlocadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLocadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
