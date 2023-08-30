d7e2e05e7556510dda621889c53b8d6e6947f23b
Amanda Merelis <amanda.fmer@gmail.com>
2023-08-30 16:04:02 -0300

    Criação dos modelos de Ator e Diretor

---
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/model/Diretor.java
@@ -0,0 +1,14 @@
+package com.locadora.backendlocadora.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Diretor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+}
