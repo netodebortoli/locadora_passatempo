4d1075c01f47157d6ae0604d9ea9f7857bce4d09
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-09-05 10:42:46 -0300

    feat: criado métodos no service/controller para editar, criar e deletar um ator.

--- backend-locadora/src/main/java/com/locadora/backendlocadora/controller/AtorController.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/controller/AtorController.java
@@ -1,20 +1,15 @@
 package com.locadora.backendlocadora.controller;
 
 import com.locadora.backendlocadora.entity.Ator;
-import com.locadora.backendlocadora.repositories.AtorRepository;
 import com.locadora.backendlocadora.service.AtorService;
+import com.locadora.backendlocadora.service.exception.NegocioException;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/atores")
@@ -27,14 +22,30 @@ public class AtorController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Ator>> listarTodos() {
-        return ResponseEntity.ok().body(atorService.buscarTodos());
+    public List<Ator> listarTodos() {
+        return atorService.buscarTodos();
     }
 
     @GetMapping("/{id}")
-    public Optional<Ator> buscarPorId(@PathVariable @NotNull @Positive Long id) {
+    public Ator buscarPorId(@PathVariable @NotNull @Positive Long id) {
         return atorService.buscarPorId(id);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ator criarAtor(@RequestBody @Valid @NotNull Ator ator) {
+        return atorService.criar(ator);
+    }
+
+    @PutMapping("/{id}")
+    public Ator atualizarAtor(@PathVariable @Positive @NotNull Long id, @RequestBody @Valid @NotNull Ator ator) {
+        return atorService.atualizar(id, ator);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deletarAtor(@PathVariable @Positive @NotNull Long id) throws NegocioException {
+        atorService.deletar(id);
+    }
 
 }
--- backend-locadora/src/main/java/com/locadora/backendlocadora/service/AtorService.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/service/AtorService.java
@@ -1,27 +1,60 @@
 package com.locadora.backendlocadora.service;
 
 import com.locadora.backendlocadora.entity.Ator;
+import com.locadora.backendlocadora.entity.Titulo;
 import com.locadora.backendlocadora.repositories.AtorRepository;
+import com.locadora.backendlocadora.repositories.TituloRepository;
+import com.locadora.backendlocadora.service.exception.NegocioException;
+import com.locadora.backendlocadora.service.exception.RegistroNaoEncontradoException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AtorService {
 
     private AtorRepository atorRepository;
 
-    public AtorService(AtorRepository atorRepository) {
+    private final TituloRepository tituloRepository;
+    private String humanReadableName = "Ator";
+
+    public AtorService(AtorRepository atorRepository, TituloRepository tituloRepository) {
         this.atorRepository = atorRepository;
+        this.tituloRepository = tituloRepository;
     }
 
     public List<Ator> buscarTodos() {
         return atorRepository.findAll();
     }
 
-    public Optional<Ator> buscarPorId(Long id) {
-        return atorRepository.findById(id);
+    public Ator buscarPorId(@Positive @NotNull Long id) {
+        return atorRepository.findById(id)
+                .orElseThrow(() -> new RegistroNaoEncontradoException(humanReadableName, id));
+    }
+
+    public Ator criar(@Valid @NotNull Ator ator) {
+        return atorRepository.save(ator);
+    }
+
+    public Ator atualizar(@Valid @NotNull Long id, @Valid @NotNull Ator ator) {
+        return atorRepository.findById(id)
+                .map(atorAtualizado -> {
+                    atorAtualizado.setNome(ator.getNome());
+                    return atorRepository.save(atorAtualizado);
+                }).orElseThrow(() -> new RegistroNaoEncontradoException(humanReadableName, id));
+    }
+
+    public void deletar(@Valid @NotNull Long id) throws NegocioException {
+//        List<Titulo> titulos = tituloRepository.findTituloByAtor(id);
+//        if (!titulos.isEmpty()) {
+//            throw new NegocioException("O Ator possui títulos associados.");
+//        }
+        atorRepository.delete(atorRepository.findById(id)
+                .orElseThrow(() -> new RegistroNaoEncontradoException(humanReadableName, id)));
     }
 
 }
