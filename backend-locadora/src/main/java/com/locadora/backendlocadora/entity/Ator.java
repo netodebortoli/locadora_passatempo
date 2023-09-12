5284b344baaf4aa5a4c4f95051ccdcc6cefa2384
Amanda Merelis <amanda.fmer@gmail.com>
2023-09-11 23:10:51 -0300

    fix: Adicionada JsonProperty no atributo 'id'

--- backend-locadora/src/main/java/com/locadora/backendlocadora/entity/Ator.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/entity/Ator.java
@@ -1,6 +1,7 @@
 package com.locadora.backendlocadora.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -19,12 +20,14 @@ public class Ator {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("_id")
     private Long id;
 
     @NotBlank
     @Column(nullable = false)
     private String nome;
 
+    @Transient
     @JsonIgnore
     @ManyToMany(mappedBy = "atores", fetch = FetchType.LAZY)
     private List<Titulo> titulos = new ArrayList<>();
