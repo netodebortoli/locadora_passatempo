d6ac909bfa221c5d332928a97d1b86aa127b349f
Amanda Merelis <amanda.fmer@gmail.com>
2023-09-12 22:40:24 -0300

    refactor: Adicionada anotação "@Transactional"

--- backend-locadora/src/main/java/com/locadora/backendlocadora/service/AtorService.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/service/AtorService.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Service;
 import com.locadora.backendlocadora.domain.Ator;
 import com.locadora.backendlocadora.domain.dto.AtorDTO;
 import com.locadora.backendlocadora.domain.mapper.AtorMapper;
-import com.locadora.backendlocadora.repositories.AtorRepository;
+import com.locadora.backendlocadora.repository.AtorRepository;
 import com.locadora.backendlocadora.service.exception.NegocioException;
 import com.locadora.backendlocadora.service.exception.RegistroNaoEncontradoException;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AtorService extends GenericService<AtorDTO, Long, AtorRepository, Ator, AtorMapper> {
 
     public AtorService(AtorRepository repository, AtorMapper atorMapper) {
