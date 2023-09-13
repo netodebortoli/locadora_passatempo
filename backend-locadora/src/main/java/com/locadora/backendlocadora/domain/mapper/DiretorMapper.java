6670eb3a03c27ac10b986931bd8df1b8c564de6c
Amanda Merelis <amanda.fmer@gmail.com>
2023-09-12 22:34:39 -0300

    feat: Criação de Mapper para Diretor

---
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/domain/mapper/DiretorMapper.java
@@ -0,0 +1,23 @@
+package com.locadora.backendlocadora.domain.mapper;
+
+import com.locadora.backendlocadora.domain.Diretor;
+import com.locadora.backendlocadora.domain.dto.DiretorDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DiretorMapper extends GenericMapper<DiretorDTO, Diretor> {
+    @Override
+    public DiretorDTO toDTO(Diretor registro) {
+        if (registro == null) return null;
+        return new DiretorDTO(registro.getId(), registro.getNome());
+    }
+
+    @Override
+    public Diretor toEntity(DiretorDTO registro) {
+        if (registro == null) return null;
+        Diretor entity = new Diretor();
+        if (registro.id() != null) entity.setId(registro.id());
+        entity.setNome(registro.nome());
+        return entity;
+    }
+}
