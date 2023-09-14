69675a80f61e3802874a61d7c9f92768f749807b
Amanda Merelis <amanda.fmer@gmail.com>
2023-09-14 11:14:41 -0300

    feat: Criação de roteamento de Classes

--- frontend-locadora/src/app/modulos/classes/classes-routing.module.ts
+++ frontend-locadora/src/app/modulos/classes/classes-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClasseFormComponent } from './containers/classe-form/classe-form.component';
+import { ClassesComponent } from './containers/classes/classes.component';
+import { classeResolver } from './guards/classe.resolver';
+
+const routes: Routes = [
+  { path: '', component: ClassesComponent },
+  {
+    path: 'novo',
+    component: ClasseFormComponent,
+    resolve: { classe: classeResolver },
+  },
+  {
+    path: 'editar/:id',
+    component: ClasseFormComponent,
+    resolve: { classe: classeResolver },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ClassesRoutingModule { }
+export class ClassesRoutingModule {}
