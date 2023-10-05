3c5422dfa68986faa88c0398e63a275e122dc74d
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-10-05 11:24:16 -0300

    refactor: ajustes no generic mapper

--- backend-locadora/src/main/java/com/locadora/backendlocadora/domain/mapper/GenericMapper.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/domain/mapper/GenericMapper.java
@@ -1,8 +1,9 @@
 package com.locadora.backendlocadora.domain.mapper;
 
+import com.locadora.backendlocadora.service.exception.NegocioException;
+
 public abstract class GenericMapper<M, E> {
 
     public abstract M toDTO(E registro);
-    public abstract E toEntity(M registro);
-
+    public abstract E toEntity(M registro) throws NegocioException;
 }
