43649618c56e41200a6b82cbf5097e775bed3599
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-10-16 20:10:04 -0300

    fix: removido JsonFormat do dto de item, em que nao permitia cadastrar atráves do front-end

--- backend-locadora/src/main/java/com/locadora/backendlocadora/domain/Item.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/domain/Item.java
@@ -2,7 +2,6 @@ package com.locadora.backendlocadora.domain;
 
 import java.sql.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
@@ -12,7 +11,6 @@ public record Item(
                 @JsonProperty("_id") Long id,
                 @NotBlank(message = "O campo Número de Série é obrigatório.") String numSerie,
                 @NotBlank(message = "O campo Tipo de Item é obrigatório.") String tipoItem,
-                @NotNull(message = "A Data de Aquisição é obrigatório.") 
-                @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") Date dataAquisicao,
+                @NotNull(message = "A Data de Aquisição é obrigatório.") Date dataAquisicao,
                 @NotNull(message = "O campo Título é obrigatório.") Titulo titulo) {
 }
