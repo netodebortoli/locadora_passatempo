ad39870eb44f6a1a206f98fc1d530afa7f34f19c
Amanda Merelis <amanda.fmer@gmail.com>
2023-10-18 16:44:13 -0300

    refactor: Atualização da página Home

--- frontend-locadora/src/app/shared/components/home/home.component.ts
+++ frontend-locadora/src/app/shared/components/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { AppMaterialModule } from '../../app-material/app-material.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppMaterialModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
