fed928f0aea618c96e91ae4171db814372b03da4
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-10-25 16:24:13 -0300

    refactor: ajustes no swagger do controle genérico

--- backend-locadora/src/main/java/com/locadora/backendlocadora/controller/GenericController.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/controller/GenericController.java
@@ -3,6 +3,9 @@ package com.locadora.backendlocadora.controller;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
@@ -25,19 +28,40 @@ public abstract class GenericController<K, M, E, MP extends GenericMapper<M, E>,
     @Autowired
     protected S service;
 
-    @Operation(description = "Obtem uma lista de todos os objetos desta entidade.")
+    @Operation(description = "Obtem uma lista de todos os objetos desta entidade.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista dos objetos desta entidade.", content = {
+                    @Content(mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Erro ao obter lista de objetos da entidade.", content = {
+                    @Content(mediaType = "application/json")
+            })
+    })
     @GetMapping
     public List<M> listarTodos() {
         return service.listarTodos();
     }
 
-    @Operation(description = "Obtem um objeto desta entidade buscando pelo ID passado na requisição.")
+    @Operation(description = "Obtem um objeto desta entidade buscando pelo ID passado na requisição.", responses = {
+            @ApiResponse(responseCode = "200", description = "Objeto encontrado.", content = {
+                    @Content(mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar os dados da entidade.", content = {
+                    @Content(mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "404", description = "Objeto não encontrado.", content = {
+                    @Content(mediaType = "application/json")
+            })
+    })
     @GetMapping("/{id}")
     public M buscarPorId(@PathVariable @NotNull @Positive K id) {
         return service.buscarPorId(id);
     }
 
-    @Operation(description = "Exclui o objeto desta entidade pelo ID passado na requisição.")
+    @Operation(description = "Exclui o objeto desta entidade pelo ID passado na requisição.", responses = {
+            @ApiResponse(responseCode = "204", description = "Objeto excluido com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Ocorreu um erro ao excluir."),
+            @ApiResponse(responseCode = "404", description = "Objeto não encontrado.")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable @Positive @NotNull K id) throws NegocioException {
