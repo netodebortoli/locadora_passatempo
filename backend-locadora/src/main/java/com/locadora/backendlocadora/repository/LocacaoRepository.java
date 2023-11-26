655b710c863df13727fb10100bb7a4740c0febea
Aristídes Debortoli Neto <netodebortoli77@gmail.com>
2023-11-25 21:28:22 -0300

    feat: implementado validações no service de locação

--- backend-locadora/src/main/java/com/locadora/backendlocadora/repository/LocacaoRepository.java
+++ backend-locadora/src/main/java/com/locadora/backendlocadora/repository/LocacaoRepository.java
@@ -1,10 +1,20 @@
 package com.locadora.backendlocadora.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.locadora.backendlocadora.domain.entity.LocacaoEntity;
 
 public interface LocacaoRepository extends JpaRepository<LocacaoEntity, Long> {
-    
-    // TODO: fazer busca de locaçoes para descobrir se o cliente está em débito
+
+    @Query("SELECT CASE WHEN COUNT(L) > 0 THEN true ELSE false END FROM LocacaoEntity L" +
+            " WHERE cliente.id = :id AND dataDevolucaoEfetiva IS null AND dataDevolucaoPrevista < now() ")
+    public boolean isClienteEmDebito(@Param("id") Long id);
+
+    @Query("From LocacaoEntity WHERE item.id = :idItem AND dataLocacao = :dataLocacao AND dataDevolucaoEfetiva IS null")
+    public LocacaoEntity isItemDisponivel(@Param("dataLocacao") Date dataLocacao, @Param("idItem") Long idItem);
+
 }
